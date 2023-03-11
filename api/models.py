005a21172c407d08073653b675e7bc6a0474514d
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-11 14:36:16 +0500

    replace auto_now

--- api/models.py
+++ api/models.py
@@ -9,8 +9,8 @@ class SmartPhone(models.Model):
     name = models.CharField(max_length=30)
     model = models.CharField(max_length=30)
 
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
